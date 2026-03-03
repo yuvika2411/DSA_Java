282497e07be280f6f31e86cbf537bd0a0799a471
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-03-04 00:26:14 +0530

    DAA

---
+++ src/Sorting/RadixSort.java
@@ -0,0 +1,57 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class RadixSort {
+
+    static int getMax(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
+
+    static void countSort(int arr[], int n, int pos) {
+        int output[] = new int[n];
+        int count[] = new int[10];
+
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / pos) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / pos) % 10] - 1] = arr[i];
+            count[(arr[i] / pos) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    static void radixSort(int arr[], int n) {
+        int max = getMax(arr, n);
+
+        for (int pos = 1; max / pos > 0; pos = pos * 10)
+            countSort(arr, n, pos);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]= new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        radixSort(arr,n);
+
+        System.out.print(arr[0]);
+        for(int i=0;i<n;i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
---
+++ src/Sorting/TimSort.java
@@ -0,0 +1,56 @@
+package Sorting;
+
+public class TimSort {
+    static final int RUN = 32;
+
+    static void insertionSort(int arr[], int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    static void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1, n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+        for (int i = 0; i < n2; i++) R[i] = arr[m + 1 + i];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2)
+            arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    static void timSort(int arr[], int n) {
+
+        for (int i = 0; i < n; i += RUN)
+            insertionSort(arr, i, Math.min(i + RUN - 1, n - 1));
+
+        for (int size = RUN; size < n; size *= 2)
+            for (int left = 0; left < n; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min(left + 2 * size - 1, n - 1);
+                if (mid < right)
+                    merge(arr, left, mid, right);
+            }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 8, 4, 2, 7, 1};
+        timSort(arr, arr.length);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
